2fa896fd70e7086964312e31ade3e4dfc733585a
岛田悠米 <115274737@qq.com>
2021-04-11 12:53:27 +0800

    基础 finish day7

---
+++ src/com/easy/day7/BFS.java
@@ -0,0 +1,28 @@
+package com.easy.day7;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BFS {
+    //图的宽度优先遍历
+    public static void bfs(Node node) {
+        if (node == null) {
+            return;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        HashSet<Node> map = new HashSet<>();
+        queue.add(node);
+        map.add(node);
+        while (!queue.isEmpty()) {
+            Node cur = queue.poll();
+            System.out.println(cur.value);
+            for (Node next : cur.nexts) {
+                if (!map.contains(next)) {
+                    map.add(next);
+                    queue.add(next);
+                }
+            }
+        }
+    }
+}
